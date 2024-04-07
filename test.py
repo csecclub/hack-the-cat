cef786b1b7f8a2266cad438258c236814e4c57b0
Betim Hodza <106614572+Betim-Hodza@users.noreply.github.com>
2024-04-07 15:36:29 -0500

    Feat: Discord bot now sends files to user if they scream SEND THE FLAGS in their dms

--- test.py
+++ test.py
@@ -7,6 +7,8 @@ load_dotenv()
 
 TOKEN = os.environ.get('DISCORD_TOKEN')
 
+
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
@@ -16,8 +18,18 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        if message.content == 'ping':
-            await message.channel.send('pong')
+        if message.content == 'SEND THE FLAGS':
+            await self.send_ctf_challenges(message.author)
+            
+    async def send_ctf_challenges(self, user):
+        # CTF challenges
+        challenge_files = ['csec_wireshark.pcap', 'tcp.pcap']
+
+        for file_name in challenge_files:
+            file_path = os.path.join('ctf/', file_name)
+            with open(file_path, 'rb') as file:
+                await user.send(file=discord.File(file, filename=file_name))
+                print(f'sent {file_name} to {user.name}')
 
 intents = discord.Intents.default()
 
