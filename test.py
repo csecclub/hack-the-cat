f9c5ae47e6e9c7fe9e0ea7af756fd34adc4c418f
Betim Hodza <106614572+Betim-Hodza@users.noreply.github.com>
2024-04-07 16:23:21 -0500

    Update: Added some dialogue to the ctf stuff

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import asyncio
 
 from dotenv import load_dotenv
 
@@ -18,9 +19,28 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        if message.content == 'SEND THE FLAGS':
+        if message.content == 'FREE':
+            self.start_here = True
+            await message.author.send("You have successfully submitted 'FREE'!")
+            await asyncio.sleep(.5)  # Wait for 1/2 seconds
+            await message.author.send("If this was the real challenge the flag would be formated like this: ")
+            await asyncio.sleep(.5)  # Wait for 1/2 seconds
+            await message.author.send("csec{FREE} ฅ(^•ﻌ•^ฅ)")
+            await asyncio.sleep(.5)  # Wait for 1/2 seconds
+            await message.author.send("make sure to submit what is inside the {} (=^・^=)")
+            await asyncio.sleep(.5)  # Wait for 1/2 seconds
+            await message.author.send("When ready enter: {START HERE}")
+
+        if message.content == "START HERE" and self.start_here:
             await self.send_ctf_challenges(message.author)
+            self.start_here = False
             
+    async def start_here(self, user, message):
+
+        if message.content == 'START HERE':
+            await self.send_ctf_challenges(user)
+        
+
     async def send_ctf_challenges(self, user):
         # CTF challenges
         challenges = [
@@ -40,6 +60,7 @@ class MyClient(discord.Client):
             with open(file_path, 'rb') as file:
                 embed = discord.Embed(title = challenge['title'],)
                 await user.send(embed = embed)
+                await asyncio.sleep(.5)  # Wait for 1/2 seconds
                 await user.send(file = discord.File(file, filename = challenge['filename']))
 
                 print(f'Sent {challenge["filename"]} to {user.name}')
