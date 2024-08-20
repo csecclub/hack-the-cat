df1f961d7c1612c1ed5793c8266509c48185af5a
Betim H <106614572+Betim-Hodza@users.noreply.github.com>
2024-08-19 22:07:25 -0500

    UPDATE: updated all questions, couldn't figure out how to send out all answers, but i made it so the questions were easy!

--- HackerCat.py
+++ HackerCat.py
@@ -72,38 +72,60 @@ class MyClient(discord.Client):
                 'Question': "What's the hidden flag?(csec{xxxxx})",
             },
             {
-                'filename': 'splitTCP.pcap',
-                'title': 'Wireshark: TCP 2',
-                'Question': 'Whats the hidden flag?(bayFLAG{xxxx})',
+                'filename': 'Meta.jpg',
+                'title': 'META: Test your abilities to extract metadata.',
+                'Question 1': 'When was the image created? Round down to the nearest minute [format: YYYY:MM:DD:MM:SS]',
+                'Question 2': 'What are the dimensions of the image? (ex: 800x600)',
+                'Question 3': 'What is the make of the camera that took the picture?',
+                'Question 4': 'What is the make of the camera that took the picture?',
+                'Question 5': 'What is the make of the camera that took the picture?',
             },
             {
-                'filename': 'DNS1.pcap',
-                'title': 'Wireshark: DNS 1',
-                'Question 1': 'What is the type of the DNS query requested?',
-                'Question 2': "What domain was requested?",
-                'Question 3': "How many items were in the response?",
-                'Question 4': "What is the TTL for all of the DNS records?\n(note that this is the TTL for the DNS record, not the IP packet.)\n(also can answer using{# h}{# m}{# s})",
-                'Question 5': "What is the IP address for the 'welcome' subdomain?",
+                'filename': '',
+                'title': 'Cryptography: Our analysts have obtained password dumps storing hacker passwords. After obtaining a few plaintext passwords, it appears that they are all simply encoded using different number bases.',
+                'Question 1': 'Patrick: 0x73636f7270696f6e',
+                'Question 2': 'Stevie: c2NyaWJibGU=',
+                'Question 3': 'Nan: 01110011 01100101 01100011 01110101 01110010 01100101 01101100 01111001',
+                'Question 4': 'Molly: 01100010 01000111 00111001 01110011 01100010 01000111 01101100 01110111 01100010 00110011 01000001 00111101',
             },
             {
-                'filename': 'HTTP1.pcap',
-                'title': 'Wireshark: HTTP 1',
-                'Question 1': 'What Linux tool was used to execute a file download?',
-                'Question 2': "What is the name of the web server software that handled the request?",
-                'Question 3': "What IP address initiated request?",
-                'Question 4': "What is the IP address of the server?",
-                'Question 5': "What is the md5sum of the file downloaded?",
+                'filename': 'git_backup.zip',
+                'title': 'Version control[git]:',
+                'Question 1': 'What is the email address of the employee who was compromised?',
+                'Question 2': "Each employee is assigned a flag. What is the flag that was compromised?",
+                'Question 3': "Greg thinks that he may have had additional account credentials that were compromised. What's the name of the service provider for that other compromised account?",
+                'Question 4': "What was the password on that compromised account?",
+            },
+            {
+                'filename': 'auth.log',
+                'title': 'Log Analysis: Analyze this ssh log file to answer the following questions.',
+                'Question 1': 'What is the hostname of the ssh server that was compromised?',
+                'Question 2': "What was the first IP address to attack the server?",
+                'Question 3': "What was the second IP address to attack the server?",
+                'Question 4': "What was the third IP address to attack the server?",
+                'Question 5': "Which user was targeted in the attack?",
+                'Question 6': "From which IP address was the attacker able to successfully log in?",
             },
         ]
 
         for challenge in challenges:
-            file_path = os.path.join('ctf/', challenge['filename'])
-            with open(file_path, 'rb') as file:
+            # if theres not file, then don't send one, if there is, then do
+            if challenge['filename'] != "":
+                file_path = os.path.join('ctf/', challenge['filename'])
+                with open(file_path, 'rb') as file:
+                    embed = discord.Embed(title=challenge['title'])
+                    await user.send(embed=embed)
+                    await asyncio.sleep(0.5)  # Wait for 1/2 seconds
+
+                    # if there's a blank file, don't send anything
+                    await user.send(file=discord.File(file, filename=challenge['filename']))
+                    print(f'Sent {challenge["filename"]} to {user.name}')
+            else:
                 embed = discord.Embed(title=challenge['title'])
                 await user.send(embed=embed)
                 await asyncio.sleep(0.5)  # Wait for 1/2 seconds
-                await user.send(file=discord.File(file, filename=challenge['filename']))
-                print(f'Sent {challenge["filename"]} to {user.name}')
+                
+                
 
             questions = [q for q in challenge if q.startswith('Question')]
             for question_num, question in enumerate(questions, start=1):
@@ -118,13 +140,15 @@ class MyClient(discord.Client):
                         self.user_scores[user.id] += 1  # Increment score for the user
                         await user.send(f"✅ Correct answer for question {question_num}! +1")
                         await user.send(f"Your score: {self.user_scores[user.id]}")
-                        print(f"Score: {user.id} | {self.user_scores[user.id]}")
 
                 while not self.is_answer_correct(challenge, question, answer.content):
                     await user.send(f"❌ Incorrect answer for question {question_num}. Please try again.")
                     answer = await self.wait_for_answer(user)
                     if self.is_answer_correct(challenge, question, answer.content):
                         await user.send(f"✅ Correct answer for question {question_num}!")
+
+        await user.send(f'CTF Completed! come get your prize!')
+        print(f'{user.name} Has completed all the CTFS')
                 
 
     async def wait_for_answer(self, user):
@@ -133,32 +157,56 @@ class MyClient(discord.Client):
         return await self.wait_for('message', check=check)
 
     def is_answer_correct(self, challenge, question, answer):
+        
         if challenge['title'] == "Wireshark: TCP 1" and question == "Question":
             return answer == "csec{1N_2040_AI_wi11_D3BU6_our_C0de}"
-        elif challenge['title'] == "Wireshark: TCP 2" and question == "Question":
-            return answer == "bayFLAG{H4v3_y0u_c53ck3d_0uT_M0BI?}"
-        elif challenge['title'] == "Wireshark: DNS 1":
+        
+        elif challenge['title'] == "META: Test your abilities to extract metadata.":
             if question == "Question 1":
-                return answer == "AXFR"
+                return answer == "2015:05:15:02:14"
             elif question == "Question 2":
-                return answer == "etas.com"
+                return answer == "1024x768"
             elif question == "Question 3":
-                return answer == "4"
+                return answer == "Apple"
             elif question == "Question 4":
-                return answer == "1 h" or answer == "3600 s" or answer == "60 m"
+                return answer == "Apple iPhone 5"
             elif question == "Question 5":
-                return answer == "1.1.1.1"
-        elif challenge['title'] == "Wireshark: HTTP 1":
+                return answer == "1/640"
+            
+        elif challenge['title'] == "Cryptography: Our analysts have obtained password dumps storing hacker passwords. After obtaining a few plaintext passwords, it appears that they are all simply encoded using different number bases.":
             if question == "Question 1":
-                return answer == "wget"
+                return answer == "scorpion"
             elif question == "Question 2":
-                return answer == "nginx"
+                return answer == "scribble"
             elif question == "Question 3":
-                return answer == "192.168.1.140"
+                return answer == "securely"
             elif question == "Question 4":
-                return answer == "174.143.213.184"
+                return answer == "lollipop"
+            
+        elif challenge['title'] == "Version control[git]:":
+            if question == "Question 1":
+                return answer == "gpeterson@mpd.hacknet.cityinthe.cloud"
+            elif question == "Question 2":
+                return answer == "-SKY-LRHX-4910"
+            elif question == "Question 3":
+                return answer == "Facebook" or answer == "FB" or answer == "facebook"
+            elif question == "Question 4":
+                return answer == "waffles85" 
+        
+        elif challenge['title'] == "Log Analysis: Analyze this ssh log file to answer the following questions.":
+            if question == "Question 1":
+                return answer == "myraptor"
+            elif question == "Question 2":
+                return answer == "169.139.243.218"
+            elif question == "Question 3":
+                return answer == "56.13.188.38"
+            elif question == "Question 4":
+                return answer == "30.167.206.91" 
             elif question == "Question 5":
-                return answer == "966007c476e0c200fba8b28b250a6379"
+                return answer == "harvey" 
+            elif question == "Question 6":
+                return answer == "30.167.206.91"
+
         else:
             return False
 
